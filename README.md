59134fa1e3102aa43a39389fe69ea2974f8c032c
Bariona <bariona@qq.com>
2024-04-27 10:21:36 +0800

    clean code

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Toy LIMA
 
 This is the repo for the toy LIMA project(homework 2 for cs296:Large Language Model in SJTU).
+
+## Dataset Generation
+
+see `./data/README.md`
+
+## Evaluation
+
+```
+alpaca_eval evaluate --model_outputs='path/to/output.json' --annotators_config='chatgpt' 
+```
+
+See the `./final_eval` to see the model's output:
+
+
+```
+final_eval
+├── Qwen0.5B_baseline.json
+├── Qwen0.5B_improved_sft.json
+├── Qwen0.5B_naive_sft.json
+├── Qwen1.8B_baseline.json
+├── Qwen1.8B_improved_sft.json
+└── Qwen1.8B_naive_sft.json
+```
--- result/clean.py
+++ result/clean.py
@@ -1,14 +1,19 @@
 import json
 
-file_name = 'baseline.json'
+for i in range(6):
+    file_name = f'{i}.json'
 
-with open(file_name, 'r', encoding='utf-8') as file:
-    data = json.load(file)
+    with open(file_name, 'r', encoding='utf-8') as file:
+        data = json.load(file)
 
-for item in data:
-    instr = item['instruction'] + ' '
-    if item['output'].startswith(instr):
-        item['output'] = item['output'].replace(instr, '', 1)
+    for item in data:
+        instr = item['instruction'] + ' '
+        if item['output'].startswith(instr):
+            item['output'] = item['output'].replace(instr, '', 1)
+        
+        instr = item['instruction']
+        if item['output'].startswith(instr):
+            item['output'] = item['output'].replace(instr, '', 1)
 
-with open(file_name, 'w', encoding='utf-8') as file:
-    json.dump(data, file, ensure_ascii=False, indent=4)
+    with open(file_name, 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
