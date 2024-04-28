038324d14fb94c8691d1b7dedec6201902df78d5
Binglin Zhou <zhoubinglin@sjtu.edu.cn>
2024-04-28 16:38:25 +0800

    Docs:update readme

--- tmp.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen1.5-0.5B")
-model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen1.5-0.5B")
-
-model.save_pretrained("Qwen/Qwen1.5-0.5B")
-tokenizer.save_pretrained("Qwen/Qwen1.5-0.5B")
